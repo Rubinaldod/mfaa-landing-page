3ec6f744f7df239b940379f2ac060c4a2394723d
Rubinaldo Domingos <RUBINALDO.DOMINGOS@unitel.co.ao>
2026-05-28 12:00:07 +0100

    update

--- src/lib/strapi/config.ts
+++ src/lib/strapi/config.ts
@@ -1,16 +1,5 @@
-const STRAPI_BASE_URL = process.env.STRAPI_BASE_URL;
-const STRAPI_API_TOKEN = process.env.STRAPI_API_TOKEN;
-
-if (!STRAPI_BASE_URL) {
-  throw new Error("Missing environment variable: STRAPI_BASE_URL");
-}
-
-if (!STRAPI_API_TOKEN) {
-  throw new Error("Missing environment variable: STRAPI_API_TOKEN");
-}
-
 export const strapiConfig = {
-  apiBaseUrl: `${STRAPI_BASE_URL}/api`,
-  token: STRAPI_API_TOKEN,
-  baseUrl: STRAPI_BASE_URL,
+  apiBaseUrl: `${process.env.STRAPI_BASE_URL ?? ""}/api`,
+  token: process.env.STRAPI_API_TOKEN ?? "",
+  baseUrl: process.env.STRAPI_BASE_URL ?? "",
 } as const;
